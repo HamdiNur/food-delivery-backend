a0f28bed580b4adabbd32484c1a95eff06efb182
HamdiNur <hamdoonur26@gmail.com>
2025-02-24 18:44:37 +0300

    Added food listing and deletion functionalities

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -3,8 +3,56 @@ import fs from 'fs';
 
 // add food item
 
-const addFood=async()=>{
+const addFood=async(req,res)=>{
+    let image_filename=`${req.file.filename}`;
+    const food=new foodModal({
+        name:req.body.name,
+        description:req.body.description,
+        price:req.body.price,
+        category:req.body.category,
+        image:image_filename
+
+    })
+    try {
+        await food.save();
+        res.json({success:true,message:"Food Added"})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:"Error"})
+        
+    }
 
 }
 
-export {addFood}
+//all food list
+const listFood=async(req,res)=>{
+    try {
+        const foods=await foodModal.find({});
+        res.json({success:true,data:foods})
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+        
+    }
+
+    
+}
+
+//remove food item
+
+const removeFood=async(req,res)=>{
+try {
+    const food=await foodModal.findById(req.body.id);
+    fs.unlink(`uploads/${food.image}`,()=>{})
+
+    await foodModal.findByIdAndDelete(req.body.id);
+    res.json({success:true,message:"Food Removed"})
+    
+} catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+}
+}
+
+export {addFood,listFood,removeFood}
