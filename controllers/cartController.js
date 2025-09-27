65720bd6cc3ea77446210cab4b09c89106cfec2b
HamdiNur <hamdoonur26@gmail.com>
2025-09-27 21:05:32 +0300

    Setup cart controller & route placeholders

---
+++ controllers/cartController.js
@@ -0,0 +1,24 @@
+import userModel from "../models/userModel.js";
+
+//Add items to user Cart
+
+const addToCart=async(req,res)=>{
+
+}
+
+
+//remove items fro user Cart 
+
+const removeFromCart=async(req,res)=>{
+
+}
+
+//Fetch user data
+
+
+const getCart=async(req,res)=>{
+
+}
+
+
+export {addToCart,removeFromCart,getCart}
