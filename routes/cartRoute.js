4e441f10243e2899818c0445968cfdd2859c1771
HamdiNur <hamdoonur26@gmail.com>
2025-09-29 22:27:01 +0300

    Add cart routes

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,11 +1,9 @@
 import express from "express"
 import { addToCart, getCart, removeFromCart } from "../controllers/cartController.js";
-
-const userRouter=express.Router()
-
-userRouter.post("/add",addToCart)
-userRouter.post("/remove",removeFromCart)
-userRouter.post("/get",getCart)
-
+import authMiddleware from "../middleware/auth.js";
+const cartRouter = express.Router();
+cartRouter.post("/add", authMiddleware, addToCart);
+cartRouter.post("/remove", authMiddleware, removeFromCart);
+cartRouter.post("/get", authMiddleware, getCart);
 
 export default cartRouter;
